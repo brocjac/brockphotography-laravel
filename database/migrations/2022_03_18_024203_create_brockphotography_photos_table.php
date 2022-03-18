d98c7b612a379770dffe0e23a7226acb56809a8c
brocjac <brockwell2001@gmail.com>
2022-03-17 22:21:56 -0500

    initial commit

---
+++ database/migrations/2022_03_18_024203_create_brockphotography_photos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brockphotography_photos', function (Blueprint $table) {
+            $table->id();
+            $table->string('Alt');
+            $table->string('Title');
+            $table->decimal('PhotoValuePrice');
+            $table->integer('CategoryId');
+            $table->timestamps();
+        });
+
+        //once the table is created use a raw query to ALTER it and add the MEDIUMBLOB
+        DB::statement("ALTER TABLE brockphotography_photos ADD ImgSrc MEDIUMBLOB NULL");
+        DB::statement("ALTER TABLE brockphotography_photos ADD LargeImgSrc MEDIUMBLOB NULL");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brockphotography_photos');
+    }
+};
---
+++ database/seeders/PhotoTableSeeder.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BrockphotographyPhoto;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PhotoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        BrockphotographyPhoto::create([
+            'ImgSrc' => file_get_contents(__DIR__."/images/landscapes/thumbnail/1.jpg"),
+            'Alt' => 'This is a photo',
+            'Title' => 'This is a title',
+            'PhotoValuePrice' => 4.99,
+            'LargeImgSrc' => file_get_contents(__DIR__."/images/landscapes/1.jpg"),
+            'CategoryId' => 1
+        ]);
+        BrockphotographyPhoto::create([
+            'ImgSrc' => file_get_contents(__DIR__."/images/landscapes/thumbnail/2.jpg"),
+            'Alt' => 'This is a photo',
+            'Title' => 'This is a title',
+            'PhotoValuePrice' => 4.99,
+            'LargeImgSrc' => file_get_contents(__DIR__."/images/landscapes/2.jpg"),
+            'CategoryId' => 1
+        ]);
+        BrockphotographyPhoto::create([
+            'ImgSrc' => file_get_contents(__DIR__."/images/landscapes/thumbnail/3.jpg"),
+            'Alt' => 'This is a photo',
+            'Title' => 'This is a title',
+            'PhotoValuePrice' => 4.99,
+            'LargeImgSrc' => file_get_contents(__DIR__."/images/landscapes/3.jpg"),
+            'CategoryId' => 1
+        ]);
+        BrockphotographyPhoto::create([
+            'ImgSrc' => file_get_contents(__DIR__."/images/landscapes/thumbnail/4.jpg"),
+            'Alt' => 'This is a photo',
+            'Title' => 'This is a title',
+            'PhotoValuePrice' => 4.99,
+            'LargeImgSrc' => file_get_contents(__DIR__."/images/landscapes/4.jpg"),
+            'CategoryId' => 1
+        ]);
+        BrockphotographyPhoto::create([
+            'ImgSrc' => file_get_contents(__DIR__."/images/landscapes/thumbnail/5.jpg"),
+            'Alt' => 'This is a photo',
+            'Title' => 'This is a title',
+            'PhotoValuePrice' => 4.99,
+            'LargeImgSrc' => file_get_contents(__DIR__."/images/landscapes/5.jpg"),
+            'CategoryId' => 1
+        ]);
+        BrockphotographyPhoto::create([
+            'ImgSrc' => file_get_contents(__DIR__."/images/landscapes/thumbnail/6.jpg"),
+            'Alt' => 'This is a photo',
+            'Title' => 'This is a title',
+            'PhotoValuePrice' => 4.99,
+            'LargeImgSrc' => file_get_contents(__DIR__."/images/landscapes/6.jpg"),
+            'CategoryId' => 1
+        ]);
+        BrockphotographyPhoto::create([
+            'ImgSrc' => file_get_contents(__DIR__."/images/landscapes/thumbnail/7.jpg"),
+            'Alt' => 'This is a photo',
+            'Title' => 'This is a title',
+            'PhotoValuePrice' => 4.99,
+            'LargeImgSrc' => file_get_contents(__DIR__."/images/landscapes/7.jpg"),
+            'CategoryId' => 1
+        ]);
+        BrockphotographyPhoto::create([
+            'ImgSrc' => file_get_contents(__DIR__."/images/landscapes/thumbnail/8.jpg"),
+            'Alt' => 'This is a photo',
+            'Title' => 'This is a title',
+            'PhotoValuePrice' => 4.99,
+            'LargeImgSrc' => file_get_contents(__DIR__."/images/landscapes/8.jpg"),
+            'CategoryId' => 1
+        ]);
+    }
+}
