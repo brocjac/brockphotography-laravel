d8ec67b1434010fb6d75682e1ce167ee76e3f49c
brocjac <jbrockwell@my.wctc.edu>
2022-04-12 16:40:52 -0500

    started redesign single photo page

--- resources/views/shop/photo.blade.php
+++ resources/views/shop/photo.blade.php
@@ -3,21 +3,25 @@
     Brock Photography
 @endsection
 @section('content')
-<div>
-    <img src="data:image/jpeg;base64, {!! base64_encode($photo->ImgSrc) !!}" alt="{{$photo->Alt}}. " class="images">
-    <h1>{{$photo->Title}}</h1>
-    <p>{{$photo->Alt}}</p>
-    <p>Photo Value: ${{$photo->PhotoValuePrice}}</p>
-    @if(Route::has('login'))
-        @auth
-            <a href="{{route('photos.edit', $photo->id)}}" id="photo' {{$photo->id}} '" class="cartphoto">Edit</a>
-            <form action="{{url('/photos', [$photo->id])}}" method="POST">
-                @csrf
-                @method("DELETE")
-                <input type="submit" value="Delete">
-            </form>
-        @endauth
-        @else
-    @endif
+<div class="photoContainer">
+    <div class="photoContainerImg">
+    <img src="data:image/jpeg;base64, {!! base64_encode($photo->LargeImgSrc) !!}" alt="{{$photo->Alt}}. " class="imagesMoreInfo">
+    </div>
+    <div class="photoContent">
+        <h1>{{$photo->Title}}</h1>
+        <p>{{$photo->Alt}}</p>
+        <p>Photo Value: ${{$photo->PhotoValuePrice}}</p>
+        @if(Route::has('login'))
+            @auth
+                <a href="{{route('photos.edit', $photo->id)}}" id="photo' {{$photo->id}} '" class="cartphoto">Edit</a>
+                <form action="{{url('/photos', [$photo->id])}}" method="POST">
+                    @csrf
+                    @method("DELETE")
+                    <input type="submit" value="Delete">
+                </form>
+            @endauth
+            @else
+        @endif
+    </div>
 </div>
 @endsection
